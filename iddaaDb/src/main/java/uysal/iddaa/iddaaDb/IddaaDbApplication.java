80d6b8b6a879d5d2ac6b6307f1bf951b36e63ec2
OkanUysal <uysal.okan.2010@gmail.com>
2022-09-21 21:04:02 +0300

    add spring project

---
+++ iddaaDb/src/main/java/uysal/iddaa/iddaaDb/IddaaDbApplication.java
@@ -0,0 +1,13 @@
+package uysal.iddaa.iddaaDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IddaaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IddaaDbApplication.class, args);
+	}
+
+}
---
+++ iddaaDb/src/test/java/uysal/iddaa/iddaaDb/IddaaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package uysal.iddaa.iddaaDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IddaaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
