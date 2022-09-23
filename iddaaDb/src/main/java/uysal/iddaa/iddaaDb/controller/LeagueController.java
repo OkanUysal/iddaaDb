3e1fb48648259b36fc2f41470c8b1b89dc3ecb30
OkanUysal <uysal.okan.2010@gmail.com>
2022-09-24 00:29:45 +0300

    create add new League rest-api

--- iddaaDb/src/main/java/uysal/iddaa/iddaaDb/controller/LeagueController.java
+++ iddaaDb/src/main/java/uysal/iddaa/iddaaDb/controller/LeagueController.java
@@ -2,12 +2,19 @@ package uysal.iddaa.iddaaDb.controller;
 
 import java.util.List;
 
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import uysal.iddaa.iddaaDb.model.League;
+import uysal.iddaa.iddaaDb.service.CountryRepository;
 import uysal.iddaa.iddaaDb.service.LeagueRepository;
 
 @RestController
@@ -15,6 +22,9 @@ public class LeagueController {
 
 	@Autowired
 	private LeagueRepository leagueRepository;
+	
+	@Autowired
+	private CountryRepository countryRepository;
 
 	@GetMapping(value = "/leagues")
 	public List<Object> getLeagues() {
@@ -29,5 +39,15 @@ public class LeagueController {
 	@GetMapping(value = "/leagueName/{name}")
 	public League getLeagueByName(@PathVariable String name) {
 		return leagueRepository.findByName(name);
+	}
+	
+	@PostMapping(value = "addLeague")
+	public League addNewLeague(@RequestBody String input) {
+		JsonObject object = new JsonParser().parse(input).getAsJsonObject();
+		League league = new League();
+		league.setName(object.get("name").getAsString());
+		league.setCountry(countryRepository.findById(object.get("country_id").getAsLong()).get());
+		
+		return leagueRepository.save(league);
 	}
 }
--- iddaaDb/src/main/java/uysal/iddaa/iddaaDb/model/League.java
+++ iddaaDb/src/main/java/uysal/iddaa/iddaaDb/model/League.java
@@ -1,16 +1,12 @@
 package uysal.iddaa.iddaaDb.model;
 
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
